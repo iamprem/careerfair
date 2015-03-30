39d69a172f73a696bdc9909e0691ee418f6e47a9
Rohit Ruparel <rpruparel@live.com>
2015-03-30 19:26:34 -0400

    WorkAuths Table commit code

--- 49erCareerFair/src/edu/uncc/database/DatabaseOpenHelper.java
+++ 49erCareerFair/src/edu/uncc/database/DatabaseOpenHelper.java
@@ -4,33 +4,35 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseOpenHelper extends SQLiteOpenHelper{
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
 	public DatabaseOpenHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
 		// TODO Auto-generated constructor stub
 	}
-	static final String DB_NAME ="mycompany.db";
+
+	static final String DB_NAME = "mycompany.db";
 	static final int DB_VERSION = 6;
+
 	@Override
-	
 	public void onCreate(SQLiteDatabase db) {
-		
+
 		CompanyTable.onCreate(db);
 		MajorsTable.onCreate(db);
 		PositionsTable.onCreate(db);
 		DegreesTable.onCreate(db);
+		WorkAuthsTable.onCreate(db);
 
-		
 	}
+
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
+
 		CompanyTable.onUpgrade(db, oldVersion, newVersion);
 		MajorsTable.onUpgrade(db, oldVersion, newVersion);
 		PositionsTable.onUpgrade(db, oldVersion, newVersion);
 		DegreesTable.onUpgrade(db, oldVersion, newVersion);
-		
+		WorkAuthsTable.onUpgrade(db, oldVersion, newVersion);
+
 	}
-	
 
 }
